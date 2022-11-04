6c4e095d1867a5055926ee1167786d11adb41355
Lamber <957742501@qq.com>
2022-11-04 23:51:19 +0800

    update step7

---
+++ exercise/step7/pointer.c
@@ -0,0 +1,43 @@
+//
+// Created by Lamber on 2022/11/4.
+//
+#include <stdio.h>
+
+/*
+ * Name : Lamber
+ * Program to experiment with hexadecimal
+ * and pointers
+ */
+
+int main(){
+    int a = 0;
+    int b = 5;
+    int c = 0x2251;
+    int *pA = &a;
+    int *pB = &b;
+    int *p;
+
+    printf("%d: %x\n", a, a);
+    printf("%d: %x\n", b, b);
+    printf("%d: %x\n", c, c);
+    printf("%lx\n", (long int)&a);
+    printf("a=%d, pA=%x, *pA=%d\n", a, (int)pA, *pA);
+
+    a = 47;
+    printf("a=%d, pA=%x, *pA=%d\n", a, (int)pA, *pA);
+    *pA = 99;
+    printf("a=%d, pA=%x, *pA=%d\n", a, (int)pA, *pA);
+
+    printf("Next experiment:\n");
+    p = pA;
+    *p = 22;
+    p = pB;
+    *p = 18;
+    p = &b;
+    *p = 108;
+    p = pA;
+    *p = 2;
+    printf("a=%d, pA=%x, *pA=%d\n", a, (int)pA, *pA);
+    printf("b=%d, pB=%x, *pB=%d\n", b, (int)pB, *pB);
+    printf("p=%x, *p=%d\n", (int)p, *p);
+}
